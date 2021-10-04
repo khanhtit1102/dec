fb9f382d96c0f21e2de880b5143411fb6721e232
Nguyen Huu Khanh <nguyenkhanh87@gmail.com>
2021-10-04 14:42:47 +0700

    Update banner link

--- resources/views/classic/banner.blade.php
+++ resources/views/classic/banner.blade.php
@@ -1,9 +1,13 @@
 <div class="bannerpc d-none d-md-block">
     <div class="banner position-absolute banner-left">
+      <a href="https://dec.tnu.edu.vn/tin-tuc/trung-tam-dao-tao-tu-xa-dai-hoc-thai-nguyen-tich-cuc-chu-dong-trong-phong-chong-dai-dich-covid-19-gop-phan-thuc-hien-thang-loi-nhiem-vu-nam-hoc-2021-2022">
         <img class="img-fluid" src="{{ asset('images/banner/1.png') }}" alt="banner trái Trung tâm Đào tào từ xa">
+      </a>  
     </div>
     <div class="banner position-absolute banner-right">
+      <a href="https://dec.tnu.edu.vn/tin-tuc/trung-tam-dao-tao-tu-xa-dai-hoc-thai-nguyen-tich-cuc-chu-dong-trong-phong-chong-dai-dich-covid-19-gop-phan-thuc-hien-thang-loi-nhiem-vu-nam-hoc-2021-2022">
         <img class="img-fluid" src="{{ asset('images/banner/1.png') }}" alt="banner phải Trung tâm Đào tào từ xa">
+      </a>
     </div>
 </div>
 <div class="banner-mobile">
@@ -11,7 +15,9 @@
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
           <div class="modal-content">
             <div class="modal-body">
+              <a href="https://dec.tnu.edu.vn/tin-tuc/trung-tam-dao-tao-tu-xa-dai-hoc-thai-nguyen-tich-cuc-chu-dong-trong-phong-chong-dai-dich-covid-19-gop-phan-thuc-hien-thang-loi-nhiem-vu-nam-hoc-2021-2022">
               <img class="img-fluid" src="{{ asset('images/banner/banner-mobile.png') }}" alt="Banner Mobile Trung tâm Đào tạo từ xa - ĐHTN">
+              </a>
             </div>
           </div>
         </div>
